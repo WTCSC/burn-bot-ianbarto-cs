54a2e41399d81995dbe7be102a18232f205de932
Ian Barto <treuxxz@icloud.com>
2025-08-28 07:18:13 -0600

    Burnbot file
    Permissions to commit changes were screwed up, so uploading file manually

---
+++ burnbot.py
@@ -0,0 +1,51 @@
+import time
+name = input("What is your name")
+
+if name == ("Ian"):
+    print("My king")
+else:
+    print(f"With a name like {name}, I am surprised that you did not come out of a mental institution.")
+    time.sleep(2)
+    print("Come back when you have something better to show for yourself")
+    
+
+time.sleep(3)
+
+Year = input("What year were you born?")
+
+if int(Year) <= 2010:
+    print("You really think that you are all of that, well you young asf")
+    time.sleep(2)
+    print("Get out of your mothers womb and come back to me when you grow a pair")
+
+else:
+    print("You really gonna try and climb your way out, get a job and start living")
+
+time.sleep(3)
+
+color = input("What is your favorite color?")
+if color.lower == "blue":
+    print("You could have chosen something unique but nooooo")
+elif color.lower == "magenta":
+    print("Atleast you are starting to have some unique taste, stop being you...")
+else:
+    print(f"Still not pleased with your color choice of {color} but it could have been worse so what you need to learn from this is to just do better...")
+
+
+
+
+
+sport = input("Name one sport that you play...")
+time.sleep(2)
+print("Atleast you are doing a sport, the only thing you and your parents can be proud of.")
+time.sleep(2)
+print("Your traits look like you might be doomed to the year 67, but if you make some changes, you might just be able to come back from this.")
+
+print("""
+ ____  ____  _      _____ _____ _      ____  _     ____ _____ _____   _     _  _____ _____   ____ _     ____  _  ____ _____ ____ 
+/   _\/  _ \/ \  /|/__ __Y  __// \__/|/  __\/ \   /  _ Y__ __Y  __/  / \   / \/    //  __/  /   _Y \ /|/  _ \/ \/   _Y  __// ___\
+|  /  | / \|| |\ ||  / \ |  \  | |\/|||  \/|| |   | / \| / \ |  \    | |   | ||  __\|  \    |  / | |_||| / \|| ||  / |  \  |    \
+|  \__| \_/|| | \||  | | |  /_ | |  |||  __/| |_/\| |-|| | | |  /_   | |_/\| || |   |  /_   |  \_| | ||| \_/|| ||  \_|  /_ \___ |
+\____/\____/\_/  \|  \_/ \____\\_/  \|\_/   \____/\_/ \| \_/ \____\  \____/\_/\_/   \____\  \____|_/ \|\____/\_/\____|____\\____/
+                                                                                                                                 
+""")
