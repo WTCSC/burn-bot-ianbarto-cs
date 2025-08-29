ad1094c19d31b7e61d909405ade113a46badc91e
ianbarto-cs <treuxxz@icloud.com>
2025-08-29 10:13:35 -0600

    changes

--- burnbot.py
+++ burnbot.py
@@ -24,15 +24,39 @@ else:
 time.sleep(3)
 
 color = input("What is your favorite color?")
+if color == "blue":
+    print("You could have chosen something unique but nooooo")
+elif color == "magenta":
+    print("Atleast you are starting to have some unique taste, stop being you...")
+elif color == "purple":
+    print("Atleast you are normal, you could haved chosen something like cyan")
+else:
+    print(f"Still not pleased with your color choice of {color} but it could have been worse so what you need to learn from this is to just do better...")
 
-
-
-
-
-
-
+time.sleep(2)
 sport = input("Name one sport that you play...")
+if sport == "tennis":
+    print("Like a proud father, good job")
+elif sport == "hockey":
+    print("you do not belong as a functioning member of society")
+else:
+    time.sleep(2)
+    print("Atleast you are doing a sport, the only thing you and your parents can be proud of.")
+    time.sleep(2)
+    print("Your traits look like you might be doomed to the year 67, but if you make some changes, you might just be able to come back from this.")
+
+food = input("What is your favorite food?")
+if food == "ribeye":
+    print("You might suck at everything else, but atleast the food choice was above par...")
+else:
+    print("""
+ ____  ____  _      _____ _____ _      ____  _     ____ _____ _____   _     _  _____ _____   ____ _     ____  _  ____ _____ ____ 
+/   _\/  _ \/ \  /|/__ __Y  __// \__/|/  __\/ \   /  _ Y__ __Y  __/  / \   / \/    //  __/  /   _Y \ /|/  _ \/ \/   _Y  __// ___\
+|  /  | / \|| |\ ||  / \ |  \  | |\/|||  \/|| |   | / \| / \ |  \    | |   | ||  __\|  \    |  / | |_||| / \|| ||  / |  \  |    \
+|  \__| \_/|| | \||  | | |  /_ | |  |||  __/| |_/\| |-|| | | |  /_   | |_/\| || |   |  /_   |  \_| | ||| \_/|| ||  \_|  /_ \___ |
+\____/\____/\_/  \|  \_/ \____\\_/  \|\_/   \____/\_/ \| \_/ \____\  \____/\_/\_/   \____\  \____|_/ \|\____/\_/\____|____\\____/
+                                                                                                                                 
+    """)
+
 time.sleep(2)
-print("Atleast you are doing a sport, the only thing you and your parents can be proud of.")
-time.sleep(2)
-print("Your traits look like you might be doomed to the year 67, but if you make some changes, you might just be able to come back from this.")
+print("Overall, your choices in life kinda suck, so make sure to contemplate them well and hard. You look like a wombat defeater that just got rabies from a bat. Do better or get cooked by some 6,7 dum dum.")
